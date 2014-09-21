e5dc3f617dcd180bbee3d61357bb609485999c5a
Jesse Kipp <esbe.v2@gmail.com>
2014-09-21 00:14:35 -0400

    add padding to mapped crime area

--- app/models/crime.rb
+++ app/models/crime.rb
@@ -1,14 +1,22 @@
 class Crime < ActiveRecord::Base
 
+  DELTA = 0.005
+
   scope :srs, -> {
     where(crimeType: ['MURDER', 'RAPE', 'ROBBERY', 'FELONY ASSAULT'])
   }
 
   def self.in_rect p1, p2
-    north, south = [p1[:lat], p2[:lat]].min, [p1[:lat], p2[:lat]].max
+    north, south = [p1[:lat], p2[:lat]].max, [p1[:lat], p2[:lat]].min
     east, west = [p1[:lng], p2[:lng]].min, [p1[:lng], p2[:lng]].max
+
+    north += DELTA
+    south -= DELTA
+    west += DELTA
+    east -= DELTA
+
     self
-      .where("crimes.latitude BETWEEN ? AND ?", north, south)
+      .where("crimes.latitude BETWEEN ? AND ?", south, north)
       .where("crimes.longitude BETWEEN ? AND ?", east, west)
   end
 
