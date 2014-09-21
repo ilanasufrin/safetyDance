4b3af39974e4e8d9287dbf988c93a0ac8b7b10ec
wentin <zhangwenting111@gmail.com>
2014-09-21 12:36:30 -0400

    add text, style change

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,6 @@
-(function(){
-  
-})();
+
+jQuery( document ).ready(function( $ ) {
+	$('#searchButton').click(function(){
+		$(this).removeClass('fui-search').addClass('glyphicon glyphicon-refresh')
+	})
+});
