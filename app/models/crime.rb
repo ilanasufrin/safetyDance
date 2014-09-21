31e720cc2d55ed7fe0705f337b2211012666c0cf
Jesse Kipp <esbe.v2@gmail.com>
2014-09-21 10:41:38 -0400

    Callback to find relevent users on a new crime

--- app/models/crime.rb
+++ app/models/crime.rb
@@ -6,6 +6,8 @@ class Crime < ActiveRecord::Base
     where(crimeType: ['MURDER', 'RAPE', 'ROBBERY', 'FELONY ASSAULT'])
   }
 
+  after_create :contact_interested_users
+
   def self.in_rect p1, p2
     north, south = [p1[:lat], p2[:lat]].max, [p1[:lat], p2[:lat]].min
     east, west = [p1[:lng], p2[:lng]].min, [p1[:lng], p2[:lng]].max
@@ -26,6 +28,12 @@ class Crime < ActiveRecord::Base
       longitude: longitude,
       type: crimeType
     }
+  end
+
+  def contact_interested_users
+    InterestPoint.around(self).pluck(:email).each do |email|
+      puts "We should notify #{email}"
+    end
   end
 
 end
--- app/models/interest_point.rb
+++ app/models/interest_point.rb
@@ -6,4 +6,17 @@ class InterestPoint < ActiveRecord::Base
   validates :longitude, presence: true
   validates :user_id, presence: true
 
+  def self.around point, delta = 0.0025
+
+    north = point.latitude + delta
+    south = point.latitude - delta
+    east = point.longitude - delta
+    west = point.longitude + delta
+
+    self.includes(:user)
+      .where(latitude: south..north)
+      .where(longitude: east..west)
+      .distinct
+  end
+
 end
